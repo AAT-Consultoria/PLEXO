2b4fdf36260c759fdcd4b8d0ce9b56b85b5648d6
AAT-Consultoria <joseluis.villen@aatconsultoria.com>
2026-09-09 10:39:33 +0200

    Update aat_rfx.user.js

--- aat_rfx.user.js
+++ aat_rfx.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @updateURL    https://github.com/AAT-Consultoria/PLEXO/raw/refs/heads/main/aat_rfx.user.js
-// @downloadURL  https://github.com/AAT-Consultoria/PLEXO/raw/refs/heads/main/aat_rfx.user.js
+// @updateURL    https://raw.githubusercontent.com/AAT-Consultoria/PLEXO/main/aat_rfx.user.js
+// @downloadURL  https://raw.githubusercontent.com/AAT-Consultoria/PLEXO/main/aat_rfx.user.js
 // @name         AAT · Gestionar RFx (Connected Supplier)
 // @namespace    https://aatconsultoria.com/
 // @version      19.0.1
